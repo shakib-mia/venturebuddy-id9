a078215a980d55ff23cd4887d9f45a8add288b19
Shakib Mia <smdshakibmia2001@gmail.com>
2022-09-21 22:40:06 +0600

    failed cases are fixed

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,67 +1,154 @@
-import React from 'react'
+import React from "react";
+import App from "./../App";
 
 const FAQ = () => {
   return (
-    <div className='faq'>
+    <div className="faq">
       <div className="d-flex title justify-content-center">
-      Frequently Asked Questions
+        Frequently Asked Questions
       </div>
 
       <div class="accordion accordion-flush" id="accordionFlushExample">
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="flush-headingOne">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-      What are the investment terms?
-      </button>
-    </h2>
-    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">content</div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="flush-headingOne">
+            <button
+              class="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseOne"
+              aria-expanded="false"
+              aria-controls="flush-collapseOne"
+            >
+              What are the investment terms?
+            </button>
+          </h2>
+          <div
+            id="flush-collapseOne"
+            class="accordion-collapse collapse"
+            aria-labelledby="flush-headingOne"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div class="accordion-body">
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Aliquid hic incidunt rem mollitia cupiditate praesentium officia
+                nobis libero facilis quos.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="flush-headingTwo">
+            <button
+              class="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseTwo"
+              aria-expanded="false"
+              aria-controls="flush-collapseTwo"
+            >
+              How does the Venturebuddy valuation tool work?
+            </button>
+          </h2>
+          <div
+            id="flush-collapseTwo"
+            class="accordion-collapse collapse"
+            aria-labelledby="flush-headingTwo"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div class="accordion-body">
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque
+                hic cumque voluptas animi dolorum qui fugit debitis quis sed
+                amet.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="flush-headingThree">
+            <button
+              class="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseThree"
+              aria-expanded="false"
+              aria-controls="flush-collapseThree"
+            >
+              Is there a sample report or a free trial available?
+            </button>
+          </h2>
+          <div
+            id="flush-collapseThree"
+            class="accordion-collapse collapse"
+            aria-labelledby="flush-headingThree"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div class="accordion-body">
+              {" "}
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero
+                doloremque facere placeat. Omnis suscipit perferendis est amet
+                et. Ipsam, nisi!
+              </p>{" "}
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="flush-headingfour">
+            <button
+              class="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapsefour"
+              aria-expanded="false"
+              aria-controls="flush-collapsefour"
+            >
+              What is the authenticity of the tool?
+            </button>
+          </h2>
+          <div
+            id="flush-collapsefour"
+            class="accordion-collapse collapse"
+            aria-labelledby="flush-headingfour"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div class="accordion-body">
+              {" "}
+              Based on the data provided by you, we use globally accepted
+              scientific methods and various metrics to carry out the valuation.{" "}
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="flush-headingfive">
+            <button
+              class="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapsefive"
+              aria-expanded="false"
+              aria-controls="flush-collapsefive"
+            >
+              How does Venturebuddy protect my data?
+            </button>
+          </h2>
+          <div
+            id="flush-collapsefive"
+            class="accordion-collapse collapse"
+            aria-labelledby="flush-headingfive"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div class="accordion-body">
+              {" "}
+              Based on the data provided by you, we use globally accepted
+              scientific methods and various metrics to carry out the valuation.{" "}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="flush-headingTwo">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-      How does the Venturebuddy valuation tool work? Accordion Item #2
-      </button>
-    </h2>
-    <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">content</div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="flush-headingThree">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-      Is there a sample report or a free trial available?
-      </button>
-    </h2>
-    <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body"> content </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="flush-headingfour">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapsefour" aria-expanded="false" aria-controls="flush-collapsefour">
-      What is the authenticity of the tool?
-      </button>
-    </h2>
-    <div id="flush-collapsefour" class="accordion-collapse collapse" aria-labelledby="flush-headingfour" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body"> Based on the data provided by you, we use globally accepted scientific methods and various metrics to carry out the valuation. </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="flush-headingfive">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapsefive" aria-expanded="false" aria-controls="flush-collapsefive">
-      How does Venturebuddy protect my data?
-      </button>
-    </h2>
-    <div id="flush-collapsefive" class="accordion-collapse collapse" aria-labelledby="flush-headingfive" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body"> Based on the data provided by you, we use globally accepted scientific methods and various metrics to carry out the valuation. </div>
-    </div>
-  </div>
-</div>
-      
-    </div>
-  )
-}
+  );
+};
 
-export default FAQ
+export default FAQ;
--- src/components/Live.js
+++ src/components/Live.js
@@ -1,67 +1,80 @@
-import React from 'react'
-import Deals from './subcomponents/Deals'
-import cover from './utils/Cover.svg'
-import profile from './utils/Profile.svg'
+import React from "react";
+import Deals from "./subcomponents/Deals";
+import cover from "./utils/Cover.svg";
+import profile from "./utils/Profile.svg";
 
 const Live = () => {
-  const deals=[{
-    cover:cover,
-    profile:profile,
-    title:"Uber eats",
-    content:"Uber Eats is an online food ordering and delivery platform launched by Uber in 2014.",
-    btn1:"FINTECH",
-    btn2:"HEALTH"
-  },{
-    cover:cover,
-    profile:profile,
-    title:"Uber eats",
-    content:"Uber Eats is an online food ordering and delivery platform launched by Uber in 2014.",
-    btn1:"FINTECH",
-    btn2:"HEALTH"
-  },{
-    cover:cover,
-    profile:profile,
-    title:"Uber eats",
-    content:"Uber Eats is an online food ordering and delivery platform launched by Uber in 2014.",
-    btn1:"FINTECH",
-    btn2:"HEALTH"
-  },{
-    cover:cover,
-    profile:profile,
-    title:"Uber eats",
-    content:"Uber Eats is an online food ordering and delivery platform launched by Uber in 2014.",
-    btn1:"FINTECH",
-    btn2:"HEALTH"
-  },{
-    cover:cover,
-    profile:profile,
-    title:"Uber eats",
-    content:"Uber Eats is an online food ordering and delivery platform launched by Uber in 2014.",
-    btn1:"FINTECH",
-    btn2:"HEALTH"
-  },{
-    cover:cover,
-    profile:profile,
-    title:"Uber eats",
-    content:"Uber Eats is an online food ordering and delivery platform launched by Uber in 2014.",
-    btn1:"FINTECH",
-    btn2:"HEALTH"
-  },]
+  const deals = [
+    {
+      cover: cover,
+      profile: profile,
+      title: "Uber eats",
+      content:
+        "Uber Eats is an online food ordering and delivery platform launched by Uber in 2014.",
+      btn1: "FINTECH",
+      btn2: "HEALTH",
+    },
+    {
+      cover: cover,
+      profile: profile,
+      title: "Uber eats",
+      content:
+        "Uber Eats is an online food ordering and delivery platform launched by Uber in 2014.",
+      btn1: "FINTECH",
+      btn2: "HEALTH",
+    },
+    {
+      cover: cover,
+      profile: profile,
+      title: "Uber eats",
+      content:
+        "Uber Eats is an online food ordering and delivery platform launched by Uber in 2014.",
+      btn1: "FINTECH",
+      btn2: "HEALTH",
+    },
+    {
+      cover: cover,
+      profile: profile,
+      title: "Uber eats",
+      content:
+        "Uber Eats is an online food ordering and delivery platform launched by Uber in 2014.",
+      btn1: "FINTECH",
+      btn2: "HEALTH",
+    },
+    {
+      cover: cover,
+      profile: profile,
+      title: "Uber eats",
+      content:
+        "Uber Eats is an online food ordering and delivery platform launched by Uber in 2014.",
+      btn1: "FINTECH",
+      btn2: "HEALTH",
+    },
+    {
+      cover: cover,
+      profile: profile,
+      title: "Uber eats",
+      content:
+        "Uber Eats is an online food ordering and delivery platform launched by Uber in 2014.",
+      btn1: "FINTECH",
+      btn2: "HEALTH",
+    },
+  ];
   return (
-    <div className='livedeals'>
+    <div className="livedeals">
       <div className="title">Live Deals</div>
       <div className="sub-title">130 Opportunities.</div>
 
       <div className="row">
-
-          {deals.map((deal)=>
-          <Deals deal={deal} closed={false}/>)}
+        {deals.map((deal) => (
+          <Deals deal={deal} closed={false} />
+        ))}
       </div>
       <div className="d-flex justify-content-center">
-        <div className="btn btn-outline-primary">View more</div>
+        <button className="btn-outline-dark-blue">View more</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Live
+export default Live;
